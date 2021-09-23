2c65e4759b4ea2fd8d11a6507eec83321adbdc90
romain leboiteux <romain@MacBook-Air-de-romain.local>
2021-09-23 15:40:58 +0200

    wordsList random

---
+++ js/pendu.js
@@ -0,0 +1,28 @@
+// Bonjour
+alert("Bonjour et bienvenue sur le jeu le pendu. ")
+alert("Bonne chance")
+
+
+// variables
+let wordsList = [
+    "Lion",
+    "Voiture",
+    "Ecole",
+    "enfant",
+    "velo",
+    "Pomme"
+] 
+let wordIA = wordsList[Math.floor(Math.random()*wordsList.length)];
+console.log (wordIA);
+
+
+
+
+
+
+
+
+// reponse prompt
+
+
+
