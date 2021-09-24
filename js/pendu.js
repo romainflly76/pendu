21a25dd2778f1c4e9ea80ca0adb68fde3fbf3d58
romain leboiteux <romain@MacBook-Air-de-romain.local>
2021-09-24 16:33:55 +0200

    let&function

--- js/pendu.js
+++ js/pendu.js
@@ -1,14 +1,7 @@
 // Bonjour
 alert("Bonjour et bienvenue sur le jeu le pendu. ")
-alert("Bonne chance")
 
-
-// variables
-
-let fin = true;
-let nb_erreurs = 0;
-let nb_passes = 0;
-let nb_mots = 0;
+// ********************** variables *****************************
 
 let wordsList = [
     "Lion",
@@ -18,19 +11,18 @@ let wordsList = [
     "velo",
     "Pomme"
 ];
-// let wordIA = wordsList[Math.floor(Math.random()*wordsList.length)];
+const maxTrials = 7;
+let wordsrandom = wordsList[Math.floor(Math.random()*wordsList.length)];
+let letters =[];
+let splitword = wordsList.split(" ");
+
+//***************************FONCTIONS ***************************
+function wordUnderScores() {
+for(i = 0; i <= wordsList.length; i++); {
+   wordUnderScores[i] - letters.push("_");
+}
 
 
-let wodsListUnderscores = [
-    "_ _ _ _",
-    "_ _ _ _ _ _ _",
-    "_ _ _ _ _",
-    "_ _ _ _ _ _",
-    "_ _ _ _",
-    "_ _ _ _ _"
-];
-
-// let wordUnderscores = wodsListUnderscores[Math.floor(Math.random()*wodsListUnderscores.length)];
 
 alert(`vous avez le droit à 7 essais`)
 alert(list);
@@ -39,24 +31,21 @@ alert(list);
 
 //les fonctions
 
-function recuperer()
+// function recuperer()
 
-function debuter()
+// function debuter()
 
-function list(){
-    let lion = wordsList[0] === wodsListUnderscores[0];
-    let voiture = wordsList[1] === wodsListUnderscores[1];
-    let ecole = wordsList[2] === wodsListUnderscores[2];
-    let enfant = wordsList[3] === wodsListUnderscores[3];
-    let velo = wordsList[4] === wodsListUnderscores[4];
-    let pomme = wordsList[5] === wodsListUnderscores[5]
+// function list(){
+//     let lion = wordsList[0] === wodsListUnderscores[0];
+//     let voiture = wordsList[1] === wodsListUnderscores[1];
+//     let ecole = wordsList[2] === wodsListUnderscores[2];
+//     let enfant = wordsList[3] === wodsListUnderscores[3];
+//     let velo = wordsList[4] === wodsListUnderscores[4];
+//     let pomme = wordsList[5] === wodsListUnderscores[5]
 
-};
+// };
 
-let listUnderScores = list[Math.floor(Math.random()*list.length)];
+// let listUnderScores = list[Math.floor(Math.random()*list.length)];
 // alert(list);
 
 //Affichage prompt
-
-
-
