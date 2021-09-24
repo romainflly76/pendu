069382c3f384bf414655d37e3455eaff5be54417
romain leboiteux <romain@MacBook-Air-de-romain.local>
2021-09-24 10:22:52 +0200

    random list

--- js/pendu.js
+++ js/pendu.js
@@ -4,6 +4,12 @@ alert("Bonne chance")
 
 
 // variables
+
+let fin = true;
+let nb_erreurs = 0;
+let nb_passes = 0;
+let nb_mots = 0;
+
 let wordsList = [
     "Lion",
     "Voiture",
@@ -12,7 +18,7 @@ let wordsList = [
     "velo",
     "Pomme"
 ];
-let wordIA = wordsList[Math.floor(Math.random()*wordsList.length)];
+// let wordIA = wordsList[Math.floor(Math.random()*wordsList.length)];
 
 
 let wodsListUnderscores = [
@@ -24,27 +30,30 @@ let wodsListUnderscores = [
     "_ _ _ _ _"
 ];
 
-let wordUnderscores = wodsListUnderscores[Math.floor(Math.random()*wodsListUnderscores.length)];
+// let wordUnderscores = wodsListUnderscores[Math.floor(Math.random()*wodsListUnderscores.length)];
 
 alert(`vous avez le droit à 7 essais`)
-alert(wordUnderscores);
+alert(list);
 
 
 
+//les fonctions
 
+function recuperer()
 
+function debuter()
 
+function list(){
+    let lion = wordsList[0] === wodsListUnderscores[0];
+    let voiture = wordsList[1] === wodsListUnderscores[1];
+    let ecole = wordsList[2] === wodsListUnderscores[2];
+    let enfant = wordsList[3] === wodsListUnderscores[3];
+    let velo = wordsList[4] === wodsListUnderscores[4];
+    let pomme = wordsList[5] === wodsListUnderscores[5]
 
-// function list(){
-//     let lion = wordsList[0] === wodsListUnderscores[0];
-//     let voiture = wordsList[1] === wodsListUnderscores[1];
-//     let ecole = wordsList[2] === wodsListUnderscores[2];
-//     let enfant = wordsList[3] === wodsListUnderscores[3];
-//     let velo = wordsList[4] === wodsListUnderscores[4];
-//     let pomme = wordsList[5] === wodsListUnderscores[5]
+};
 
-// };
-
+let listUnderScores = list[Math.floor(Math.random()*list.length)];
 // alert(list);
 
 //Affichage prompt
